9028c0915269478cd604ea63945133e4c039085a
Andres Felipe <afgberrocal2504@gmail.com>
2025-08-13 12:31:12 -0500

    Mejorando sintaxis codigo3.py

--- codigo3.py
+++ codigo3.py
@@ -1,10 +1,10 @@
 nombre = input("Digita tu nombre: ")
 print(f'Hola {nombre} como te encuentras el dia de hoy?')
-print("Adios")  
+print("Hasta pronto")  
 
-n1=input("Digita un un numero")
-n2=input("Digita numeor 2")
+n1=input("Digita un un numero: ")
+n2=input("Digita numeor 2: ")
 
-result= f' El resultado de la suma es de {n1+n2}'
+result= f' La suma es = {n1+n2}'
 print(result)
 
