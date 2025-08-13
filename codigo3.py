f03f68bb4caab01fb271957e31c622e0b698a6e4
Andres Felipe <afgberrocal2504@gmail.com>
2025-08-13 12:27:11 -0500

    Agregando operacion al archivo py

--- codigo3.py
+++ codigo3.py
@@ -1,3 +1,10 @@
 nombre = input("Digita tu nombre: ")
 print(f'Hola {nombre} como te encuentras el dia de hoy?')
-print("Adios")
+print("Adios")  
+
+n1=input("Digita un un numero")
+n2=input("Digita numeor 2")
+
+result= f' El resultado de la suma es de {n1+n2}'
+print(result)
+
